b199805fe9f20b37a7b2fd3a140d5f265c7daaef
Akshay <dudaakshay@gmail.com>
2022-08-01 18:49:18 +0530

    Commiting my 2nd code

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //pen = 10; notebook = 40
+
+        int cash = sc.nextInt();
+        if(cash < 10) {
+            System.out.println("Cannot buy anything");
+            System.out.println("Get More Cash!!");
+        }
+
+        else if(cash > 10 && cash < 50) {
+            System.out.println("Can get one thing");
+        }
+
+        else{
+            System.out.println("Can get both");
+        }
+    }
+}
